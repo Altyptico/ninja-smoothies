7f78c0584ea804935c139fa3ea6b1d1611e4151b
Altyptico <54339617+Altyptico@users.noreply.github.com>
2020-03-13 13:48:42 -0400

    adding delete smoothie method to index component

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,7 +2,7 @@
   <div class="index container">
       <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
         <div class="card-content">
-          <i class="material-icons delete">delete</i>
+          <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
           <h2 class="indigo-text">{{smoothie.title}}</h2>
           <ul class="ingredients">
               <li v-for="(ing, index) in smoothie.ingredients" :key="index"> <span class="chip">{{ ing }}</span></li>
@@ -24,6 +24,13 @@ export default {
         {title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'juice'], id: 2}
       ]
     }
+    },
+    methods:{
+      deleteSmoothie(id){
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id != id
+          })
+      }
   }
 }
 </script>
